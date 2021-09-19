8261e5563e5a32c5cb09148f9d7ea9efd2f00576
maha303 <86849690+maha303@users.noreply.github.com>
2021-09-19 11:57:20 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+fun main(){
+    print("Enter your name :   ")
+   var username = readLine()
+   println("Hello $username , welcome to my program!")
+    var msg = "Hello user, welcome to my program!"
+    msg = "Hello $username , welcome to my program!"
+    println(msg)
+
+}
